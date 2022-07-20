8a315342314ca008c5e3df5a9a4c9718aaa2ee69
GiangHoGoVap <58210406+GiangHoGoVap@users.noreply.github.com>
2022-07-20 16:57:33 +0700

    Add files via upload

---
+++ src/user/user.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateUserInput, User, UserDocument } from './user.schema';
+
+@Injectable()
+export class UserService {
+    constructor(@InjectModel(User.name) private userModel: Model<UserDocument>){}
+
+    async findMany() {
+        return this.userModel.find().lean();
+    }
+
+    async findById(id) {
+        return this.userModel.findById(id).lean();
+    }
+
+    async createUser(user: CreateUserInput) {
+        return this.userModel.create(user);
+    }
+}
